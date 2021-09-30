cc5f80ab6815f481602119e679fc835eb869dc02
Your Name <you@example.com>
2021-09-30 14:31:31 -0300

    problema com movimentacao corrigido

--- script.js
+++ script.js
@@ -25,138 +25,40 @@ const argolas = document.querySelectorAll('.argola')
 
 const dropzones = document.querySelectorAll('.dropzone')
 
-<<<<<<< Updated upstream
-=======
 let dropzoneFirst = document.getElementById("dropzone-1")
 let dropzoneSecond = document.getElementById("dropzone-2")
 let dropzoneThird = document.getElementById("dropzone-3")
 
 
-
->>>>>>> Stashed changes
 argolas.forEach(argola => {
     argola.addEventListener('dragstart', dragstart)
-    argola.addEventListener('drag', drag)
     argola.addEventListener('dragend', dragend)
 })
 
 function dragstart() {
-    //console.log('Argola: Start dragging ')
+    this.classList.add('arrastando')
+}
 
-    this.classList.add('is-dragging')
-}
-function drag() {
-    //console.log('Argola: is dragging ')
-}
 function dragend() {
-    //console.log('Argola: Stop dragging ')
-
-    this.classList.remove('is-dragging')
+    this.classList.remove('arrastando')
 }
 
 
 dropzones.forEach(dropzone => {
-    dropzone.addEventListener('dragenter', dragenter)
     dropzone.addEventListener('dragover', dragover)
-    dropzone.addEventListener('dragenter', dragleave)
-    dropzone.addEventListener('drop', drop)
 })
 
-function dragenter() {
-    
-}
-    let drop1 = dropzoneFirst.children[dropzoneFirst.children.length -1]
-    let drop2 = dropzoneSecond.children[dropzoneSecond.children.length -1]
-    let drop3 = dropzoneThird.children[dropzoneThird.children.length -1]
-
 function dragover() {
-    const argolaOver = document.querySelector('.is-dragging')
-<<<<<<< Updated upstream
-    this.appendChild(argolaOver)
-
-}
-
-function dragleave() {
-=======
->>>>>>> Stashed changes
-    
-
-<<<<<<< Updated upstream
-function drop() {
-    
-}
-=======
-    console.log(this.appendChild(argolaOver).clientWidth)
-    // console.log(argolaOver.clientWidth)
-    // console.log(dropzoneSecond[dropzoneSecond.length -1].clientWidth)
-    if (dropzoneSecond.children === null || argolaOver.clientWidth < dropzoneFirst.children[dropzoneFirst.children.length -1]){
-        console.log ('Bom dia!')
-        
-        return true
-        // console.log(argolaOver.clientWidth)
-        // console.log(dropzoneSecond.lastElementChild.clientWidth)
-    // } else if (argolaOver.clientWidth < dropzoneThird === null || dropzoneThird.lastElementChild.clientWidth) {
-    //     return true
-    // } else if (argolaOver.clientWidth < dropzoneFirst === null || dropzoneFirst.lastElementChild.clientWidth) {
-    //     return true
-    // 
-    }  else if (argolaOver.clientWidth > dropzoneSecond.children[dropzoneSecond.children.length -2]) {
-        console.log('Boa noite!')
-        return false
-
+    const argolaOver = document.querySelector('.arrastando')
+    console.log (argolaOver)
+    console.log (this.children.length)
+    if (this.children.length === 0) {
+        this.appendChild(argolaOver)
+    } if (argolaOver.clientWidth < this.children[this.children.length -1].clientWidth){
+        console.log('oi')
+        this.appendChild(argolaOver)
     }
-    console.log(dropzoneSecond.children[dropzoneSecond.children.length -2])
 }
 
-function dragleave() {
-    
-}
+console.log(dropzoneFirst)
 
-function drop() {
-    
-    // const argolaDrop = document.querySelector('.is-dragging')
-    // this.appendChild(argolaDrop)
-    // if (dropzoneSecond.lastElementChild === null || argolaDrop.clientWidth < dropzoneSecond.lastElementChild.clientWidth){
-    //     console.log ('Bom dia!')
-    //     return true
-      
-    // // } else if (argolaOver.clientWidth < dropzoneThird === null || dropzoneThird.lastElementChild.clientWidth) {
-    // //     return true
-
-    // // } else if (argolaOver.clientWidth < dropzoneFirst === null || dropzoneFirst.lastElementChild.clientWidth) {
-    // //     return true
-
-    // // 
-    // }  else {
-    //     console.log('Boa noite!')
-    //     return false
-    // }
-}
-
-
-
-
-// function (){
-//     for (let i = 0; i < dropzoneSecond.childElementCount) 
-//     if (disk[i].clientWidth(elemento que estamos movendo) < 
-//                 disk[ultimoElemento-da-torre que queremos mover].clientWidth){
-//         //add elemento na torre
-//     } else {
-//         undefined
-//     }
-// }
-
-// mover da torre 1 para a torre 2;
-// if (o tamanho do elemento do elemento for menor, pode mover)
-// else (não mova)
-
-
-//verificar a quantidade de elements
-//dropzoneFirst
-//dropzoneFirst.childElementCount
-//dropzones[0]
-
-// verificar o tamanho da div
-//.clientWidth
-
->>>>>>> Stashed changes
